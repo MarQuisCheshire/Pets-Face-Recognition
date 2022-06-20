966f26b081072636fd2e23fbc6855fd183965cc9
k.kudelkin <kudkonst13@gmail.com>
2022-06-20 23:58:07 +0300

    fix imports

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,6 +1,4 @@
 from .controller import Controller
 from .detector_controller import DetectionController
 from .keypoints_controller import KeyPointsController
-from .lmd_transfer_controller import LMDTransferController
-from .segmentation_controller import SegmentationController
 from .trainer import Trainer
